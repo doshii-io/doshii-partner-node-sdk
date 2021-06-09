2bd65bdf8e8e87d547335da220774b844af83b19
Job <jobin.jj@gmail.com>
2021-06-09 13:45:21 +1000

    remove client id

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,6 @@
 import Doshii from "./doshii";
 import sampleData from "./samples";
 
-const clientId =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkb3NoaWkiLCJzdWIiOnsiZm9yIjoiQXBwQ2xpZW50SWQiLCJpZCI6bnVsbH0sImV4cCI6MTcxNzg5Mzk5NX0.Y5uNqg28Qg4aIJ3kkrfa7QS_MEaj7aVcu31-yBDdiMY";
-const clientSecret =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkb3NoaWkiLCJzdWIiOnsiZm9yIjoiQXBwQ2xpZW50U2VjcmV0IiwiaWQiOm51bGx9LCJleHAiOjE3MTc4OTM5OTV9.yFsQ2TBEXT2j3MKN6t5D62QIhB1az17QfJbr24fFris";
 
 let d = new Doshii(clientId, clientSecret, true);
 
